3570cbe4e039ff45a4479a13bfa4af09451e8057
Jevgenijs Kozlovs <Yevgeny>
2022-12-04 18:35:40 +0200

    feat: implement cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+const getPair = (list, index) => '' + list[index] + 'is' + list[index + 1];
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = [];
+    let key = ''
+    args.forEach((arg, index) => {
+        if (index % 2 === 0) key = arg.substring(2);
+        else result.push(key + ' is ' + arg);
+    });
+    console.log(result.join(', '));
 };
 
 parseArgs();
