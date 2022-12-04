d89baee522afb4a80eaaef00caa441aefdfc9de8
Jevgenijs Kozlovs <Yevgeny>
2022-12-04 19:56:36 +0200

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+
+const sourcePath = process.cwd() + '/src/hash/files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash('sha256', 'secret');
+    const readStream = fs.createReadStream(sourcePath);
+
+    readStream.on('readable', () => {
+        const data = readStream.read();
+        if (data)
+            hash.update(data);
+        else {
+            console.log(`${hash.digest('hex')}`);
+        }
+    });
 };
 
 await calculateHash();
+
