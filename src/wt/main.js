15d7029b484b8949d02aeb6a0a8a812e4fd6b10b
Jevgenijs Kozlovs <Yevgeny>
2022-12-04 22:25:46 +0200

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import os from 'node:os';
+import {
+    Worker, isMainThread, parentPort, workerData
+} from 'node:worker_threads';
+
+const coresCount = os.cpus().length;
+
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPromises = [];
+
+    for (let index = 0; index < coresCount; index++) {
+        const p = new Promise((resolve, reject) => {
+            const worker = new Worker('./src/wt/worker.js', {
+                workerData: 10 + index,
+            });
+            worker.on('message', res => {
+                resolve({ status: 'resolved', data: res });
+            });
+            worker.on('error', res => {
+                reject({ status: 'error', data: null })
+            });
+        });
+        workerPromises.push(p);
+    };
+
+    const results = await Promise.all(workerPromises);
+    console.log(results)
 };
 
 await performCalculations();
