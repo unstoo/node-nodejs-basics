7b12a4c0386dcaccedc0a4d26ff799c0ba44dead
Jevgenijs Kozlovs <Yevgeny>
2022-12-04 17:32:28 +0200

    feat: implement fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+
+const error = Error('FS operation failed');
+const sourcePath = process.cwd() + '/src/fs/files/fileToRemove.txt';
+
 const remove = async () => {
-    // Write your code here 
+    let sourceExists = false;
+
+    try {
+        sourceExists = await (await fs.stat(sourcePath)).isFile();
+    } catch (e) { }
+    if (sourceExists == false) throw error;
+
+    await fs.rm(sourcePath);
 };
 
 await remove();
+
