251db0bf8fd802bde3cda8c198d196c8720a252b
Jevgenijs Kozlovs <Yevgeny>
2022-12-05 22:17:30 +0200

    feat: implement child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,10 @@
+import * as url from 'url';
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childPath = url.fileURLToPath(new URL('.', import.meta.url)) + '/files/script.js';
+    const child = await fork(childPath, args, { stdio: 'inherit' });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([42, null]);
